8319c61f158eaf633ad0cae45c010992007fac96
yokyj <373149567@qq.com>
2017-12-16 22:12:15 +0800

    version2.0 with easy cookie

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -59,6 +59,13 @@ func DealWithResponse(res *http.Response) {
 	}
 }
 
+func CheckPanic(err error) {
+	if err != nil {
+		panic(err)
+		return
+	}
+}
+
 func init() {
 	cobra.OnInitialize(initConfig)
 
